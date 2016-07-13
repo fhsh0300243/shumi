edd8ad43cca6aa119d640f9322b46ffa333e6e35
shumi <lilisbg.lee@gmail.com>
2016-07-13 17:49:38 +0800

    add order by

--- dish/views.py
+++ dish/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import DishPost
+from django.utils import timezone
 
 def dish_home(request):
-    dish_list=DishPost.objects.all()
+    dish_list=DishPost.objects.filter(DishCreated_date__lte=timezone.now()).order_by('DishCreated_date')
     return  render(request, 'dish/dish_list.html',{'dish_list': dish_list})
 
 def dish_detail(request, pk):
--- life/views.py
+++ life/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import LifePost
+from django.utils import timezone
 
 def life_home(request):
-    life_list=LifePost.objects.all()
+    life_list=LifePost.objects.filter(LifeCreated_date__lte=timezone.now()).order_by('LifeCreated_date')
     return  render(request, 'life/life_list.html',{'life_list': life_list})
 
 def life_detail(request, pk):
