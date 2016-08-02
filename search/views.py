03ed9bc92a339727ea194ee3bab8589fb34ee8ed
shumi <lilisbg.lee@gmail.com>
2016-08-02 11:00:32 +0800

    Modify search

--- search/views.py
+++ search/views.py
@@ -2,16 +2,28 @@ from django.shortcuts import render
 from django.db.models import Q
 from dish.models import Dish_post
 from life.models import Life_post
+from functools import reduce
+import operator
 
 # Create your views here.
 def search(request):
-    query=request.GET.get('q', '')
+    global result_empty_for_dish, result_empty_for_life, errors
+    result_empty_for_dish=[]
+    result_empty_for_life=[]
+    errors = []
+    query=request.GET.get('q')
     if query:
-        qset_for_dish=Q(Dish_title__icontains=query)
-        qset_for_life=Q(Life_title__icontains=query)
-        result_for_dish=Dish_post.objects.filter(qset_for_dish)
-        result_for_life=Life_post.objects.filter(qset_for_life)
+        result_for_dish = Dish_post.objects.filter(Q(Dish_title__icontains=query))
+        result_for_life = Life_post.objects.filter(Q(Life_title__icontains=query))
+        if not result_for_dish:
+            result_empty_for_dish.append('Not found')
+        if not result_for_life:
+            result_empty_for_life.append('Not found')
+        return render(request, 'search/search.html',
+                      {'result_for_dish': result_for_dish, 'result_for_life': result_for_life, 'result_empty_for_dish':result_empty_for_dish, 'result_empty_for_life':result_empty_for_life})
     else:
-        result_for_dish=[]
-        result_for_life=[]
-    return render(request, 'search/search.html', {'result_for_dish':result_for_dish, 'result_for_life':result_for_life})
+        errors.append('請輸入搜尋項目')
+        return render(request, 'search/search.html', {'errors': errors})
+
+
+
